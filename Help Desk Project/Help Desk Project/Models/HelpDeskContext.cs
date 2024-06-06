351c2162b8d3ea25ca828e5e68c9fed7c1fc6d33
Zack Prohonic <zackprohonic20@gmail.com>
2024-06-05 21:46:42 -0400

    SQL server and entity framework scaffolding completed

---
+++ Help Desk Project/Help Desk Project/Models/HelpDeskContext.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore;
+
+namespace Help_Desk_Project.Models;
+
+public partial class HelpDeskContext : DbContext
+{
+    public HelpDeskContext()
+    {
+    }
+
+    public HelpDeskContext(DbContextOptions<HelpDeskContext> options)
+        : base(options)
+    {
+    }
+
+    public virtual DbSet<Bookmark> Bookmarks { get; set; }
+
+    public virtual DbSet<Ticket> Tickets { get; set; }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder) { }
+
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<Bookmark>(entity =>
+        {
+            entity.HasKey(e => e.BookmarkId).HasName("PK__Bookmark__541A3B71DEB5E34E");
+
+            entity.Property(e => e.UserId).HasMaxLength(50);
+
+            entity.HasOne(d => d.Ticket).WithMany(p => p.Bookmarks)
+                .HasForeignKey(d => d.TicketId)
+                .HasConstraintName("FK__Bookmarks__Ticke__398D8EEE");
+        });
+
+        modelBuilder.Entity<Ticket>(entity =>
+        {
+            entity.HasKey(e => e.TicketId).HasName("PK__Tickets__712CC6071347AD22");
+        });
+
+        OnModelCreatingPartial(modelBuilder);
+    }
+
+    partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+}
---
+++ Help Desk Project/Help Desk Project/Models/Ticket.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Help_Desk_Project.Models;
+
+public partial class Ticket
+{
+    public int TicketId { get; set; }
+
+    public string? Subject { get; set; }
+
+    public string? Details { get; set; }
+
+    public string? Resolution { get; set; }
+
+    public bool? TicketStatus { get; set; }
+
+    public virtual ICollection<Bookmark> Bookmarks { get; set; } = new List<Bookmark>();
+}
