554fba0c9fb4107d2f4e3214496a3e4aef38bec4
Jered O'Grady <jered9715@gmail.com>
2024-06-12 20:27:21 -0400

    did stuff

--- help-desk-project/src/app/components/bookmarks-list/bookmarks-list.component.ts
+++ help-desk-project/src/app/components/bookmarks-list/bookmarks-list.component.ts
@@ -6,8 +6,6 @@ import { ticket } from '../../interfaces/ticket';
 import { bookmark } from '../../interfaces/bookmark';
 import { FormsModule } from '@angular/forms';
 import { CommonModule } from '@angular/common';
-
-
 @Component({
   selector: 'app-bookmarks-list',
   standalone: true,
@@ -19,33 +17,31 @@ export class BookmarksListComponent implements OnInit {
 bookmarks: bookmark[]=[];
 bookmarksTicketIds: number[]=[];
 bookmarkedTickets: ticket[]=[];
-
-
 constructor(private ticketService: TicketsService, private bookmarkService: BookmarkService, private router: Router){}
-
   ngOnInit(): void {
     this.fetchBookmarks();
   }
-
-
   userId: string = 'random';
 fetchBookmarks(): void{
   this.bookmarkService.getBookmarks().subscribe((data: bookmark[])=>{
+    console.log('fetching bookmarks/data', data);
     this.bookmarks = data;
+    console.log('fetching bookmarks/bookmarks', this.bookmarks)
+   console.log('fetching bookmarks');
+   this.fetchBookedmarkedTicketsDetails();
   }
-
   )
 }
-
-
 //Thought process was to get the bookmarks > take out specicically the numbers > put those to a list > then use them to sort through each to get list
 //may find a easier way by using either two way binding or events
 //wish there was something like linq that we can use
 fetchBookedmarkedTicketsDetails(): void{
+  console.log('fetching bookmarks',this.bookmarks)
 this.bookmarks.forEach( bookmark=> {
-  this.bookmarksTicketIds.push(bookmark.TicketId)
+  console.log(bookmark)
+  this.bookmarksTicketIds.push(bookmark.ticketId)
 });
-
+console.log('fetching bookmark details');
 console.log(this.bookmarksTicketIds)
 this.bookmarksTicketIds.forEach(id =>{
 this.ticketService.getTicket(id).subscribe((data: ticket)=>{
@@ -53,12 +49,4 @@ this.ticketService.getTicket(id).subscribe((data: ticket)=>{
 })
 });
 }
-
-
-
-
-
-
-
-
 }
